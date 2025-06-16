50bc50aa12b94a491d42a19b7cbe3c140f3d139a
Marek Hrusovsky <xhruso00@gmail.com>
2025-06-16 23:35:10 +0800

    Using developer intended MASShortcutBinder to register defaults in demo

--- Demo/AppDelegate.m
+++ Demo/AppDelegate.m
@@ -22,14 +22,14 @@ static void *MASObservingContext = &MASObservingContext;
 
 	// Most apps need default shortcut, delete these lines if this is not your case
 	MASShortcut *firstLaunchShortcut = [MASShortcut shortcutWithKeyCode:kVK_F1 modifierFlags:NSEventModifierFlagCommand];
-    MASDictionaryTransformer *transformer = [[MASDictionaryTransformer alloc] init];
-    NSDictionary *firstLaunchShortcutDict = [transformer reverseTransformedValue:firstLaunchShortcut];
 
     // Register default values to be used for the first app start
     [defaults registerDefaults:@{
         MASHardcodedShortcutEnabledKey : @YES,
         MASCustomShortcutEnabledKey : @YES,
-		MASCustomShortcutKey : firstLaunchShortcutDict
+    }];
+    [[MASShortcutBinder sharedBinder] registerDefaultShortcuts:@{
+            MASCustomShortcutKey : firstLaunchShortcut
     }];
 
     // Bind the shortcut recorder view’s value to user defaults.
